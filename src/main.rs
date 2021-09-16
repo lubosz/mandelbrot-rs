e62205421c3935f98e50fc20f327f7049f9ee811
Lubosz Sarnecki <lubosz@gmail.com>
2021-09-16 19:01:05 +0200

    load texture

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use sdl2::pixels::Color;
+// use sdl2::pixels::Color;
+use sdl2::image::{InitFlag, LoadTexture};
 use std::time::Duration;
+use std::path::Path;
 
 pub fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
+    let _image_context = sdl2::image::init(InitFlag::PNG | InitFlag::JPG)?;
+
 
     let window = video_subsystem
         .window("Mandelbrot", 800, 600)
@@ -16,8 +20,13 @@ pub fn main() -> Result<(), String> {
 
     let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
 
-    canvas.set_draw_color(Color::RGB(255, 0, 0));
-    canvas.clear();
+    //canvas.set_draw_color(Color::RGB(255, 0, 0));
+    //canvas.clear();
+    let texture_creator = canvas.texture_creator();
+    let texture = texture_creator.load_texture(Path::new("/home/bmonkey/Pictures/chat.jpg"))?;
+
+    canvas.copy(&texture, None, None)?;
+
     canvas.present();
     let mut event_pump = sdl_context.event_pump()?;
 
@@ -33,11 +42,13 @@ pub fn main() -> Result<(), String> {
             }
         }
 
-        canvas.clear();
-        canvas.present();
+        //canvas.clear();
+        //canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30));
         // The rest of the game loop goes here...
     }
 
     Ok(())
 }
+
+
