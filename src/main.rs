abfc7496e47f9bf02ef435289702cdc04371337f
Lubosz Sarnecki <lubosz@gmail.com>
2021-09-18 01:34:52 +0200

    move color mapping to funct.

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,37 @@ fn draw(texture_canvas: &mut Canvas<Window>, img: &ImageBuffer<Rgb<f64>, Vec<f64
   }
 }
 
+fn map_color(iteration: u32, max_iteration: u32) -> Rgb::<f64> {
+  let color = iteration as f64 / max_iteration as f64;
+
+  if iteration == max_iteration {
+    Rgb::<f64>([0.0, 0.0, 0.0])  /* In the set. Assign black. */
+  } else if iteration < max_iteration / 64 {
+    let r = color * 32.0;
+    Rgb::<f64>([r, 0.0, 0.0])
+  } else if iteration < max_iteration / 32 {
+    let r = (((iteration - max_iteration/64) as f64 * 128.0/256.0) / max_iteration as f64 /32.0) + 128.0/256.0;
+    Rgb::<f64>([r, 0.0, 0.0])
+  } else if iteration < max_iteration / 16 {
+    let r = (((iteration - max_iteration/32) as f64 * 62.0/256.0) / max_iteration as f64 /32.0) + 193.0/256.0;
+    Rgb::<f64>([r, 0.0, 0.0])
+  } else if iteration < max_iteration / 8 {
+    let g = (((iteration - max_iteration/16) as f64 * 62.0/256.0) / max_iteration as f64 /16.0) + 1.0/256.0;
+    Rgb::<f64>([1.0, g, 0.0])
+  } else if iteration < max_iteration / 4 {
+    let g = (((iteration - max_iteration/8) as f64 * 63.0/256.0) / max_iteration as f64 /8.0) + 64.0/256.0;
+    Rgb::<f64>([1.0, g, 0.0])
+  } else if iteration < max_iteration / 2 {
+    let g = (((iteration - max_iteration/4) as f64 * 63.0/256.0) / max_iteration as f64 /4.0) + 128.0/256.0;
+    Rgb::<f64>([1.0, g, 0.0])
+  } else if iteration < max_iteration {
+    let g = (((iteration - max_iteration/2) as f64 * 63.0/256.0) / max_iteration as f64 /2.0) + 192.0/256.0;
+    Rgb::<f64>([1.0, g, 0.0])
+  } else {
+    Rgb::<f64>([1.0, 1.0, 0.0])
+  }
+}
+
 fn generate_image (w: u32, h: u32, max_iteration: u32) -> ImageBuffer<Rgb<f64>, Vec<f64>> {
   let mut img = ImageBuffer::<Rgb<f64>, Vec<f64>>::new(w, h);
 
@@ -48,34 +79,7 @@ fn generate_image (w: u32, h: u32, max_iteration: u32) -> ImageBuffer<Rgb<f64>, 
       iteration += 1;
     }
 
-    let color = iteration as f64 / max_iteration as f64;
-
-    if iteration == max_iteration {
-      *pixel = Rgb::<f64>([0.0, 0.0, 0.0]);  /* In the set. Assign black. */
-    } else if iteration < max_iteration / 64 {
-      let r = color * 32.0;
-      *pixel = Rgb::<f64>([r, 0.0, 0.0]);
-    } else if iteration < max_iteration / 32 {
-      let r = (((iteration - max_iteration/64) as f64 * 128.0/256.0) / max_iteration as f64 /32.0) + 128.0/256.0;
-      *pixel = Rgb::<f64>([r, 0.0, 0.0]);
-    } else if iteration < max_iteration / 16 {
-      let r = (((iteration - max_iteration/32) as f64 * 62.0/256.0) / max_iteration as f64 /32.0) + 193.0/256.0;
-      *pixel = Rgb::<f64>([r, 0.0, 0.0]);
-    } else if iteration < max_iteration / 8 {
-      let g = (((iteration - max_iteration/16) as f64 * 62.0/256.0) / max_iteration as f64 /16.0) + 1.0/256.0;
-      *pixel = Rgb::<f64>([1.0, g, 0.0]);
-    } else if iteration < max_iteration / 4 {
-      let g = (((iteration - max_iteration/8) as f64 * 63.0/256.0) / max_iteration as f64 /8.0) + 64.0/256.0;
-      *pixel = Rgb::<f64>([1.0, g, 0.0]);
-    } else if iteration < max_iteration / 2 {
-      let g = (((iteration - max_iteration/4) as f64 * 63.0/256.0) / max_iteration as f64 /4.0) + 128.0/256.0;
-      *pixel = Rgb::<f64>([1.0, g, 0.0]);
-    } else if iteration < max_iteration {
-      let g = (((iteration - max_iteration/2) as f64 * 63.0/256.0) / max_iteration as f64 /2.0) + 192.0/256.0;
-      *pixel = Rgb::<f64>([1.0, g, 0.0]);
-    } else {
-      *pixel = Rgb::<f64>([1.0, 1.0, 0.0]);
-    }
+    *pixel = map_color(iteration, max_iteration);
   }
 
   img
